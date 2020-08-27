f83668aa2fbeb7384673a3e1eccd16e0c54a4482
Daniel S Fava <danielsfava@gmail.com>
2020-08-27 13:56:30 +0200

    some variable renaming

--- cube.go
+++ cube.go
@@ -121,62 +121,62 @@ func (fl Flat) String() string {
 // We use the Vec and CVec to find the indices in Flat that need to be
 // populated, and we use the CVec to determine the string representation
 // the location's color.
-func (fl *Flat) PaintCubi(cbi Cubi, n uint) {
+func (fl *Flat) PaintCubi(cubi Cubi, n uint) {
 	var offset float64
 	if n%2 == 0 {
 		offset = 0.5
 	}
-	signX := float64(-1 * Sign(cbi.pv[Xax]))
-	signY := float64(-1 * Sign(cbi.pv[Yax]))
-	signZ := float64(-1 * Sign(cbi.pv[Zax]))
-	if cbi.cv[Xax] > 0 {
-		r := int(n/2) - int(math.Ceil(float64(cbi.pv[Zax])+signZ*offset))
-		c := int(n/2) - int(math.Ceil(float64(cbi.pv[Yax])+signY*offset))
+	signX := float64(-1 * Sign(cubi.pv[Xax]))
+	signY := float64(-1 * Sign(cubi.pv[Yax]))
+	signZ := float64(-1 * Sign(cubi.pv[Zax]))
+	if cubi.cv[Xax] > 0 {
+		r := int(n/2) - int(math.Ceil(float64(cubi.pv[Zax])+signZ*offset))
+		c := int(n/2) - int(math.Ceil(float64(cubi.pv[Yax])+signY*offset))
 		off_r := int(n)
 		off_c := int(n * 2)
-		(*fl)[r+off_r][c+off_c] = cbi.cv[Xax].Abs().String()
-	} else if cbi.cv[Xax] < 0 {
-		r := int(n/2) - int(math.Ceil(float64(cbi.pv[Zax])+signZ*offset))
-		c := int(n/2) + int(math.Floor(float64(cbi.pv[Yax])+signY*offset))
+		(*fl)[r+off_r][c+off_c] = cubi.cv[Xax].Abs().String()
+	} else if cubi.cv[Xax] < 0 {
+		r := int(n/2) - int(math.Ceil(float64(cubi.pv[Zax])+signZ*offset))
+		c := int(n/2) + int(math.Floor(float64(cubi.pv[Yax])+signY*offset))
 		off_r := int(n)
 		off_c := 0
-		(*fl)[r+off_r][c+off_c] = cbi.cv[Xax].Abs().String()
+		(*fl)[r+off_r][c+off_c] = cubi.cv[Xax].Abs().String()
 	}
-	if cbi.cv[Yax] > 0 {
-		r := int(n/2) - int(math.Ceil(float64(cbi.pv[Zax])+signZ*offset))
-		c := int(n/2) + int(math.Floor(float64(cbi.pv[Xax])+signX*offset))
+	if cubi.cv[Yax] > 0 {
+		r := int(n/2) - int(math.Ceil(float64(cubi.pv[Zax])+signZ*offset))
+		c := int(n/2) + int(math.Floor(float64(cubi.pv[Xax])+signX*offset))
 		off_r := int(n)
 		off_c := int(n)
-		(*fl)[r+off_r][c+off_c] = cbi.cv[1].Abs().String()
-	} else if cbi.cv[Yax] < 0 {
-		r := int(n/2) - int(math.Ceil(float64(cbi.pv[Zax])+signZ*offset))
-		c := int(n/2) - int(math.Ceil(float64(cbi.pv[Xax])+signX*offset))
+		(*fl)[r+off_r][c+off_c] = cubi.cv[1].Abs().String()
+	} else if cubi.cv[Yax] < 0 {
+		r := int(n/2) - int(math.Ceil(float64(cubi.pv[Zax])+signZ*offset))
+		c := int(n/2) - int(math.Ceil(float64(cubi.pv[Xax])+signX*offset))
 		off_r := int(n)
 		off_c := int(n * 3)
-		(*fl)[r+off_r][c+off_c] = cbi.cv[1].Abs().String()
+		(*fl)[r+off_r][c+off_c] = cubi.cv[1].Abs().String()
 	}
-	if cbi.cv[Zax] > 0 {
-		r := int(n/2) + int(math.Floor(float64(cbi.pv[Yax])+signY*offset))
-		c := int(n/2) + int(math.Floor(float64(cbi.pv[Xax])+signX*offset))
+	if cubi.cv[Zax] > 0 {
+		r := int(n/2) + int(math.Floor(float64(cubi.pv[Yax])+signY*offset))
+		c := int(n/2) + int(math.Floor(float64(cubi.pv[Xax])+signX*offset))
 		off_r := 0
 		off_c := int(n)
-		(*fl)[r+off_r][c+off_c] = cbi.cv[2].Abs().String()
-	} else if cbi.cv[Zax] < 0 {
-		r := int(n/2) - int(math.Ceil(float64(cbi.pv[Yax])+signY*offset))
-		c := int(n/2) + int(math.Floor(float64(cbi.pv[Xax])+signX*offset))
+		(*fl)[r+off_r][c+off_c] = cubi.cv[2].Abs().String()
+	} else if cubi.cv[Zax] < 0 {
+		r := int(n/2) - int(math.Ceil(float64(cubi.pv[Yax])+signY*offset))
+		c := int(n/2) + int(math.Floor(float64(cubi.pv[Xax])+signX*offset))
 		off_r := int(n * 2)
 		off_c := int(n)
-		(*fl)[r+off_r][c+off_c] = cbi.cv[2].Abs().String()
+		(*fl)[r+off_r][c+off_c] = cubi.cv[2].Abs().String()
 	}
 }
 
-func (fl *Flat) PaintCube(cb Cube) {
-	(*fl) = make([][]string, cb.n*3)
+func (fl *Flat) PaintCube(cube Cube) {
+	(*fl) = make([][]string, cube.n*3)
 	for idx := range *fl {
-		(*fl)[idx] = make([]string, cb.n*4)
+		(*fl)[idx] = make([]string, cube.n*4)
 	}
-	for idx := range cb.cubis {
-		fl.PaintCubi(cb.cubis[idx], cb.n)
+	for idx := range cube.cubis {
+		fl.PaintCubi(cube.cubis[idx], cube.n)
 	}
 }
 
@@ -188,11 +188,11 @@ type Matrix [3][3]int // a matrix
 //
 // The function multiplies the matrix by the position vector and
 // it multiplies the matrix by the color vector
-func (m *Matrix) Mult(cbi Cubi) Cubi {
+func (m *Matrix) Mult(cubi Cubi) Cubi {
 	var ret Cubi
 	for i := 0; i <= 2; i++ {
-		ret.pv[i] = m[i][0]*cbi.pv[0] + m[i][1]*cbi.pv[1] + m[i][2]*cbi.pv[2]
-		ret.cv[i] = Color(m[i][0]*int(cbi.cv[0]) + m[i][1]*int(cbi.cv[1]) + m[i][2]*int(cbi.cv[2]))
+		ret.pv[i] = m[i][0]*cubi.pv[0] + m[i][1]*cubi.pv[1] + m[i][2]*cubi.pv[2]
+		ret.cv[i] = Color(m[i][0]*int(cubi.cv[0]) + m[i][1]*int(cubi.cv[1]) + m[i][2]*int(cubi.cv[2]))
 	}
 	return ret
 }
@@ -208,9 +208,9 @@ type Cube struct {
 }
 
 // A string representation of a Rubik's cube
-func (cb Cube) String() string {
+func (cube Cube) String() string {
 	var fl Flat
-	fl.PaintCube(cb)
+	fl.PaintCube(cube)
 	return fl.String()
 }
 
@@ -266,13 +266,13 @@ func GetRotationMatrix(a Axis, counter bool) Matrix {
 }
 
 // Rotates a cube by multiplying the relevant little cubes by a rotation matrix
-func (cb Cube) Rotate(a Axis, idx int, counter bool) Cube {
-	ret := cb.New()
+func (cube Cube) Rotate(a Axis, idx int, counter bool) Cube {
+	ret := cube.New()
 	m := GetRotationMatrix(a, counter)
-	for cb_idx := range cb.cubis {
-		if cb.cubis[cb_idx].pv[a] == idx {
+	for cube_idx := range cube.cubis {
+		if cube.cubis[cube_idx].pv[a] == idx {
 			// We rotate via matrix multiplication
-			ret.cubis[cb_idx] = m.Mult(cb.cubis[cb_idx])
+			ret.cubis[cube_idx] = m.Mult(cube.cubis[cube_idx])
 		}
 	}
 	return ret
@@ -288,9 +288,9 @@ func (cb Cube) Rotate(a Axis, idx int, counter bool) Cube {
 // orange to the right    (x>0)
 // yellow upward          (z>0)
 // white  downward        (z<0)
-func (cb *Cube) Init(n uint) {
-	(*cb).n = n
-	(*cb).cubis = make([]Cubi, int(math.Pow(float64(n), 3)-math.Pow(float64(n)-2, 3)))
+func (cube *Cube) Init(n uint) {
+	(*cube).n = n
+	(*cube).cubis = make([]Cubi, int(math.Pow(float64(n), 3)-math.Pow(float64(n)-2, 3)))
 
 	ncubi := 0
 	for x := -int(n) / 2; x <= int(n)/2; x++ {
@@ -332,18 +332,18 @@ func (cb *Cube) Init(n uint) {
 				} else if z == -extremity {
 					zc = -white
 				}
-				cb.cubis[ncubi] = Cubi{cv: CVec{xc, yc, zc}, pv: Vec{x, y, z}}
+				cube.cubis[ncubi] = Cubi{cv: CVec{xc, yc, zc}, pv: Vec{x, y, z}}
 				ncubi += 1
 			}
 		}
 	}
 }
 
-func (cb *Cube) Shuffle(times uint) {
+func (cube *Cube) Shuffle(times uint) {
 	axes := [...]Axis{Xax, Yax, Zax}
-	idxs := make([]int, cb.n+(cb.n+1)%2)
-	for idx := -int(cb.n) / 2; idx <= int(cb.n)/2; idx++ {
-		idxs[idx+int(cb.n)/2] = idx
+	idxs := make([]int, cube.n+(cube.n+1)%2)
+	for idx := -int(cube.n) / 2; idx <= int(cube.n)/2; idx++ {
+		idxs[idx+int(cube.n)/2] = idx
 	}
 	dirs := [...]bool{true, false}
 	var perms uint
@@ -351,28 +351,28 @@ func (cb *Cube) Shuffle(times uint) {
 		ax := axes[rand.Intn(len(axes))]  // pick an axis
 		idx := idxs[rand.Intn(len(idxs))] // pick an index
 		dir := dirs[rand.Intn(len(dirs))] // pick a direction
-		if cb.n%2 == 0 && idx == 0 {
+		if cube.n%2 == 0 && idx == 0 {
 			continue
 		}
-		(*cb) = cb.Rotate(ax, idx, dir)
+		(*cube) = cube.Rotate(ax, idx, dir)
 		perms += 1
 	}
 }
 
-func (cb *Cube) IsSolved() bool {
+func (cube *Cube) IsSolved() bool {
 	// We could do with [3][2]Color, I'm wasting a bit of memory to simplify
 	// the algorithm:
 	// Signs -1 and 1 map to array indexes 0 and 2 as opposed to 0 and 1
 	var sideColor [3][3]Color
-	extremity := int((cb.n) / 2)
-	for idx := range cb.cubis {
-		//fmt.Println(cb.cubis[idx].pv, cb.cubis[idx].cv)
+	extremity := int((cube.n) / 2)
+	for idx := range cube.cubis {
+		//fmt.Println(cube.cubis[idx].pv, cube.cubis[idx].cv)
 		for _, ax := range [...]Axis{Xax, Yax, Zax} {
 			for sign := range []int{-1, 1} {
-				if cb.cubis[idx].pv[ax] == sign*extremity {
+				if cube.cubis[idx].pv[ax] == sign*extremity {
 					if sideColor[ax][sign+1] == zero {
-						sideColor[ax][sign+1] = cb.cubis[idx].cv[ax]
-					} else if sideColor[ax][sign+1] != cb.cubis[idx].cv[ax] {
+						sideColor[ax][sign+1] = cube.cubis[idx].cv[ax]
+					} else if sideColor[ax][sign+1] != cube.cubis[idx].cv[ax] {
 						return false
 					}
 				}
@@ -383,12 +383,12 @@ func (cb *Cube) IsSolved() bool {
 }
 
 // A copy-constructor
-func (cb Cube) New() Cube {
+func (cube Cube) New() Cube {
 	var ret Cube
-	ret.n = cb.n
-	ret.cubis = make([]Cubi, len(cb.cubis))
-	for idx := range cb.cubis {
-		ret.cubis[idx] = cb.cubis[idx]
+	ret.n = cube.n
+	ret.cubis = make([]Cubi, len(cube.cubis))
+	for idx := range cube.cubis {
+		ret.cubis[idx] = cube.cubis[idx]
 	}
 	return ret
 }
