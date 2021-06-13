1bc8d9c9e2d380a83ca5aba8e8f181566dbe0d63
Daniel S Fava <danielsfava@gmail.com>
2021-06-13 18:15:03 +0200

    examples/swap_corners.go

---
+++ examples/swap_corners.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Daniel S. Fava. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	. "github.com/dfava/cube"
+)
+
+func swap_top_corners() {
+	//PrintInColors(false)
+	var cb Cube
+	cb.Init(3)
+	fmt.Println(cb)
+	fmt.Println()
+
+	// Swap cubi (1,1,1) with cubi (-1,1,1)
+	cb = cb.Turn(Xax, 1, Counterclock)
+	cb = cb.Turn(Zax, -1, Counterclock)
+	cb = cb.Turn(Xax, 1, Clock)
+
+	cb = cb.Turn(Xax, -1, Counterclock)
+	cb = cb.Turn(Zax, -1, Clock)
+	cb = cb.Turn(Xax, -1, Clock)
+
+	cb = cb.Turn(Xax, 1, Counterclock)
+	cb = cb.Turn(Zax, -1, Counterclock)
+	cb = cb.Turn(Xax, 1, Clock)
+
+	fmt.Println(cb)
+	fmt.Println()
+
+	//
+	cb = cb.Turn(Zax, 1, Counterclock)
+	cb = cb.Turn(Zax, 1, Counterclock)
+	fmt.Println(cb)
+	fmt.Println()
+
+	// Reverse swap
+	cb = cb.Turn(Xax, 1, Counterclock)
+	cb = cb.Turn(Zax, -1, Clock)
+	cb = cb.Turn(Xax, 1, Clock)
+
+	cb = cb.Turn(Xax, -1, Counterclock)
+	cb = cb.Turn(Zax, -1, Counterclock)
+	cb = cb.Turn(Xax, -1, Clock)
+
+	cb = cb.Turn(Xax, 1, Counterclock)
+	cb = cb.Turn(Zax, -1, Clock)
+	cb = cb.Turn(Xax, 1, Clock)
+
+	fmt.Println(cb)
+	fmt.Println()
+
+	//
+	cb = cb.Turn(Zax, 1, Clock)
+	cb = cb.Turn(Zax, 1, Clock)
+	fmt.Println(cb)
+	fmt.Println()
+}
+
+func main() {
+	swap_top_corners()
+}
